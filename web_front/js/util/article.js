f20fda9f56b7a14c210e717032f6c77d593fd579
unknown <1419528576@qq.com>
2020-02-06 12:43:29 +0800

    在index.html获取文章类名成功

---
+++ web_front/js/util/article.js
@@ -0,0 +1,25 @@
+// 把所有与文章相关的操作写在这里
+
+var article = {
+    // 获取
+    // page 就是当前要取出来的第几页的数据
+    // type 就是当前文章的类型编号
+    // status 就是当前文章的状态
+    get: function (page, type, status) {
+        return $.get(APILIST.article_get, {
+            'page': page,
+            'type': type,
+            'state': status
+        })
+    },
+    /**
+     * 根据id去获取文章的详情
+     * @param {Number}} id 
+     */
+    getById: function (id) {
+        return $.get(APILIST.article_get, {
+            'id': id
+        })
+    },
+
+}
